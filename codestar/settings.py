242fd171ebe256df04de57250baa0d11491c19bd
martiiann <martiian03817@icloud.com>
2025-03-31 16:07:46 +0100

    Add Heroku domain to ALLOWED_HOSTS

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7xwxny-y8-7j&*ha!e&s7m#%y05u7is*mos8@tcsu9t+a8iuvh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
